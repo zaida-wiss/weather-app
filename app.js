641101bb0c9802ba58a9d92e35d5230f6fce9958
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-03 09:34:02 +0100

    gjorde en dev branch

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const MOCK_WEATHER = {
   "Göteborg": { tempC: 8, description: "Lätt regn", icon: "🌧️", updated: "09:00" },
   "Malmö": { tempC: 10, description: "Klart", icon: "☀️", updated: "09:00" },
   "Uppsala": { tempC: 6, description: "Disigt", icon: "🌫️", updated: "09:00" },
-  "Lund": { tempC: 9, description: "Halvklart", icon: "⛅", updated: "09:00" }
+  "Lund": { tempC: 9, description: "Halvklart", icon: "⛅", updated: "09:00" },
+
 };
+
 
 
 const searchBtn = document.getElementById("searchBtn");
