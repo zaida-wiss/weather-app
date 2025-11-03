58be81dd0a1c037e6539ae2b4cc541aab8a92e32
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-03 11:02:33 +0100

    dubbla js?

---
+++ väderkort.js
@@ -0,0 +1,25 @@
+{/* <body>
+    <div class="container">
+        <h1>Väderapp</h1>
+        <p>Sök efter en stad för att se aktuellt väder.</p>
+        <input id="cityInput" type="text" placeholder="Ange stad (t.ex Umeå)" />
+        <button id="searchBtn">Sök</button>
+    <div id="weatherResult" class="card hidden">
+        <h2 id="cityName"></h2>
+        <p id="temperature"></p>
+        <p id="description"></p>
+        <span id="icon"></span>
+        <p id="updatedTime"></p>
+    </div>
+    <script src="app.js"></script>
+</body>
+</html> */}
+
+const weatherResult = document.getElementById("weatherResult");
+const searchBtn = document.getElementById("searchBtn");
+const wethercard = document.createElementById("div");
+searchBtn.addEventListener("click", () =>)
+
+
+
+
