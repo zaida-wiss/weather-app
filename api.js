7866140a47afd5bcfc10042f40f4d69f85caf3c5
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-10 10:36:21 +0100

    api fungerar

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-import {getWeather} from "../data.js";
-import {renderWeather} from "../ui.js";
+import {getWeather} from "./data.js";
+import {renderWeather} from "./ui.js";
 
 const cityInput = document.getElementById("input");
 const searchBtn = document.getElementById("inputBtn");
